b2fcb991540ada4dba0015960d6f8369d9700710
Safal <safalgautam212@gmail.com>
2024-10-25 16:41:30 +0545

    first end-point created

--- profileproject/profiles_api/views.py
+++ profileproject/profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response #Response is used to return response when api view is called 
 
-# Create your views here.
+
+
+class HelloApiView(APIView):
+    """Test API View"""
+    
+    #request is passed by django
+    def get(self, request, fromat=None):
+        """Returns a list of APIView features"""
+        an_apiview=[
+            'Uses HTTP methods as function (get, post, patch , put , delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLs',
+        ] 
+        return Response({'message':'Hello!', 'an_apiview':an_apiview})
